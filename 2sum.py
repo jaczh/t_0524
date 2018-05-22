3110e11f48ee855e90951a72b0260c0667765d19
jaczh <jjsmailmail@gmail.com>
2018-05-21 21:25:48 -0700

    not modified

---
+++ 2sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+        for i, data in enumerate(nums):
+            m = target - data
+            if m in d:
+                return [d[m], i]
+            else:
+                d[data] = i
